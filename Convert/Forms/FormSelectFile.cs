8d0f1339189938d2e93408eb7693520b1fb14a90
garymiller <garymiller30@gmail.com>
2021-03-20 17:53:12 +0200

    видаляє Aspose

--- Convert/Forms/FormSelectFile.cs
+++ Convert/Forms/FormSelectFile.cs
@@ -1,13 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Aspose.Pdf.Drawing;
 using Path = System.IO.Path;
 
 namespace PDFManipulate.Forms
